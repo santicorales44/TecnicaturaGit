55dfbc0c7a7b5665191bf951e47c06b5c50c308c
Santi Corales <santicorales44@gmail.com>
2026-06-04 21:00:32 -0300

    Cerramos leccion 2 y agregamos ejercicio 1

---
+++ Python/Ejercicio 1.py
@@ -0,0 +1,4 @@
+"""
+Exercise 1:
+We have to show an expression as an algorithmic expression, in other words, make it in code
+"""
--- Python/Leccion 2.py
+++ Python/Leccion 2.py
@@ -1,1 +1,25 @@
-
+"""
+#CLASS 6 - CONTROL STATEMENTS
+#Statement if/else
+condition = 10
+if condition == True:
+    print("True Condition")
+elif condition == False:
+    print("False Condition")
+else:
+    print("No specified condition")
+"""
+"""
+#Exercise: Number to text convertion
+number = int(input("Enter a number in range of 1 to 3: "))
+number_text = ""
+if number == 1:
+    number_text = "Number one"
+elif number == 2:
+    number_text = "Number two"
+elif number == 3:
+    number_text = "Number three"
+else:
+    number_text = "Error. You have entered a number out of range."
+print(f"The entered number is: {number} - {number_text}")
+"""
