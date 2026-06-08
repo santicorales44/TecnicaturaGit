b43d9d59ad0f70fd3fb470496c33015ed8e112b0
Santi Corales <santicorales44@gmail.com>
2026-06-07 21:10:09 -0300

    Agrego Hi World from Java

---
+++ Java/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,12 @@
+//Our first program Hi World - One line commentary
+/*
+many lines, extensive commentaries
+bla
+bla
+bla
+*/
+public class HolaMundo {
+    public static void main(String args[]){
+        System.out.println("Hi world from Java!");
+    }
+}
