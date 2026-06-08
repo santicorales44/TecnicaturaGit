f5e0aa837fb50a63aac9e4cc0172650d1ab2c677
Santi Corales <santicorales44@gmail.com>
2026-06-08 00:08:42 -0300

    Agrego clase 2

--- Java/Leccion1/HolaMundo/src/HolaMundo.java
+++ Java/Leccion1/HolaMundo/src/HolaMundo.java
@@ -1,5 +1,14 @@
 public class HolaMundo {
    public static void main(String[]args){
        System.out.println("Hi world from Java");
+       int myVariable = 10;
+        System.out.println(myVariable);
+        myVariable = 5;
+        System.out.println(myVariable);
+        //String Type
+        String myVariableChain = "Welcome";
+        System.out.println(myVariableChain);
+        myVariableChain = "Let's keep growing in coding";
+        System.out.println(myVariableChain);
    } 
 }
