e6da6c5be274f1d29c1f7a4f5fbc17399f396b4e
Santi Corales <santicorales44@gmail.com>
2026-06-12 12:33:13 -0300

    Agrego clase 7 de Java

--- Java/Leccion1/HolaMundo/src/HolaMundo.java
+++ Java/Leccion1/HolaMundo/src/HolaMundo.java
@@ -1,7 +1,6 @@
 
 import java.util.Scanner;
 
-
 public class HolaMundo {
 
     public static void main(String[] args) {
@@ -65,8 +64,8 @@ public class HolaMundo {
         System.out.println("Enter the autor's name: ");
         var authorName = entrance.nextLine();
         System.out.println("The book "+bookName+" was written by " + authorName);
-        */
-        /*
+         */
+ /*
         byte intNumberByte = 127;
         System.out.println("intNumberByte = " + intNumberByte);
         System.out.println("Byte's minimum value:" + Byte.MIN_VALUE);
@@ -86,9 +85,9 @@ public class HolaMundo {
         System.out.println("intNumberLong = " + intNumberLong);
         System.out.println("Long minimum value: " + Long.MIN_VALUE);
         System.out.println("Long maximum value: " + Long.MAX_VALUE);
-        */
-        
-        /*
+         */
+
+ /*
         float floatNumber = 3.4028235E38F;
         System.out.println("floatNumber = " + floatNumber);
         System.out.println("Float's minimum value is:" + Float.MIN_VALUE);
@@ -98,9 +97,8 @@ public class HolaMundo {
         System.out.println("doubleNumber = " + doubleNumber);
         System.out.println("Double's minimum value is:" + Double.MIN_VALUE);
         System.out.println("Double's maximum value is:" + Double.MAX_VALUE);
-        */
-        
-        /*
+         */
+ /*
         //Var and primitive types inference
         var intNumber = 20;  //Literals without . are automatically int type.
         System.out.println("intNumber = " + intNumber);
@@ -131,9 +129,8 @@ public class HolaMundo {
         System.out.println("varIntegerChar = " + varIntegerChar);
         int characterChar = 's';
         System.out.println("characterChar = " + characterChar);
-        */
-        
-        /*
+         */
+ /*
         //Boolean primitive types
         var varBool = false;
         System.out.println("varBool = " + varBool);
@@ -155,9 +152,8 @@ public class HolaMundo {
         else{
             System.out.println("You are a minor");
          }
-        */
-        
-        /*
+         */
+ /*
         //Primitive types convertion
         var age = Integer.parseInt("20");
         System.out.println("age = " + (age + 1));
@@ -169,9 +165,8 @@ public class HolaMundo {
         System.out.println("Enter your age: ");
         age = Integer.parseInt(entrance.nextLine());
         System.out.println("age = " + age);
-        */
-        
-        /*
+         */
+ /*
         //Primitive types convertion Part 2
         var textAge = String.valueOf(10);
         System.out.println("textAge = " + textAge);
@@ -182,8 +177,199 @@ public class HolaMundo {
         System.out.println("Enter a character: ");
         charPhrase = entrance.nextLine().charAt(0);
         System.out.println("charPhrase = " + charPhrase);
+         */
+ /*
+        int num1 = 5, num2 = 4;
+        var solution = num1 + num2;
+        System.out.println("addition solution = " + solution);
+        
+        solution = num1 - num2;
+        System.out.println("substraction solution = " + solution);
+
+        solution = num1 * num2;
+        System.out.println("multiplication solution = " + solution);
+        
+        solution = num1 / num2;
+        System.out.println("division solution = " + solution);
+        
+        var solution2 = 3.4 / num2;
+        System.out.println("division solution = " + solution2);
+        
+        solution = num1 % num2; // It saves the integer rest of the division
+        System.out.println("solution = " + solution); // 5/4
+        
+        // IMPORTANT: If using an if/else with only one line, it's not necessary
+        // to use {}. It's an exception.
+        if (num1 % 2 == 0)
+            System.out.println("It's an even number");
+            else
+            System.out.println("It's an odd number");   
+         */
+ /*
+        int varNum1 = 1, varNum2 = 4;
+        int varNum3 = varNum1 + 6 - varNum2; // An operation
+        System.out.println("varNum3 = " + varNum3);
+        
+        varNum1 += 1; // this is the same as: varNum1 = varNum1 + 1;
+        System.out.println("varNum1 = " + varNum1);
+        
+        varNum2 -= 1;
+        System.out.println("varNum2 = " + varNum2);
+        
+        varNum2 *= 3;
+        System.out.println("varNum2 = " + varNum2);
+        
+        varNum2 /= 3;
+        System.out.println("varNum2 = " + varNum2);
+        
+        varNum1 %= 2;
+        System.out.println("varNum1 = " + varNum1);
+         */
+ /*
+        // Unary Operators: Sign change
+        var varA = 7;
+        var varB = -varA;
+        System.out.println("varA = " + varA);
+        System.out.println("varB = " + varB); // The result will be a negative number
+        
+        // Negation operator
+        var varC = true; //For default, this literal in Java is a boolean type
+        var varD = !varC; // Here it changes the value
+        System.out.println("varC = " + varC);
+        System.out.println("varD = " + varD);
+        
+        // Increase Unary Operators: 
+        //Pre-Increase
+        var varE = 9; // It's value will change
+        var varF = ++varE; // Symbol before the value
+        // First it increments the variable and then uses it's value
+        System.out.println("varE = " + varE); // It increases in the unity
+        System.out.println("varF = " + varF); // It will add one
+        
+        // Post-Increase (The symbol is used after the variable.
+        var varG = 3;
+        var varH = varG++; //First the variable value and then the increment
+        System.out.println("varG = " + varG);
+        System.out.println("varH = " + varH);
+        
+        // Decrease Unary Operators:
+        // Pre-Decrease
+        var varI = 4;
+        var varJ = --varI;
+        System.out.println("varI = " + varI);//The variable is decreased
+        System.out.println("varJ = " + varJ);
+        
+        //Post-Decrease
+        var varK = 8;
+        var varL = varK--; // First the variable value, then the decrease is left
+        System.out.println("varK = " + varK); // Here it will decrease in 1
+        System.out.println("varL = " + varL);
+         */
+ /*
+        // Equality and Relational Operators
+        
+        var aNum = 5;
+        var bNum = 4;
+        var cNum = (aNum == bNum);
+        System.out.println("cNum = " + cNum);
+
+        var dNum = aNum != bNum;
+        System.out.println("dNum = " + dNum);
+
+        var chainA = "Hello";
+        var chainB = "Bye";
+        var cVar = chainA == chainB;
+        System.out.println("cVar = " + cVar);
+
+        var fVar = chainA.equals(chainB);
+        System.out.println("fVar = " + fVar);
+
+        var gVar = aNum >= bNum; // > >= < <= == !=
+        System.out.println("gVar = " + gVar);
+
+        if (aNum % 2 == 0) {
+            System.out.println("The number is Even");
+        } else {
+            System.out.println("The number is Odd");
+        }
+
+        var age = 30;
+        var adult = 18;
+        if(age>=adult){
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are a minor");
+        }
+         */
+ 
+        /*
+        // Conditional operators: 
+        // and (&&)
+        // or (
+        var valueA = 7;
+        var minimumValue = 0; // Range between 0 and 10
+        var maximumValue = 10;
+        var answer = valueA >= 0 && valueA <= 10;
+
+        if (answer) {
+            System.out.println("It's in the established range.");
+        } else {
+            System.out.println("It's out of the established range.");
+        }
+
+        var holidays = true;
+        var freeDay = false;
+        if (holidays || freeDay) {
+            System.out.println("Dad can attend the game with his son.");
+        } else {
+            System.out.println("Dad can't attend the game with his son.");
+        }
         */
         
+        /*
+        // Ternary Operator
         
+        var resultT = (5>8)? "True" : "False";
+        System.out.println("resultT = " + resultT);
+        
+        var numberT = 7;
+        resultT = (numberT % 2 == 0) ? "It's even" : "It's odd";
+        System.out.println("resultT = " + resultT);
+        */
+        
+        /*
+        var x = 5;
+        var y = 10;
+        var z = ++x + y--;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+        
+        var arithmeticSolution = 4 + 5 * 6 / 3;
+        System.out.println("arithmeticSolution = " + arithmeticSolution);
+        
+        arithmeticSolution = (4 + 5) * 6/3;
+        System.out.println("arithmeticSolution = " + arithmeticSolution);
+        */
+        
+        /*
+          Scanner entrance = new Scanner (System.in);
+          System.out.println("Enter the rectangle's base: ");
+          double base = entrance.nextDouble();
+          System.out.println("Enter the rectangle's height");
+          double height = entrance.nextDouble();
+        
+        double area = base * height;
+        double perimeter = 2 * (base + height);
+        
+        System.out.println("The area of the rectangle is: "+area);
+        System.out.println("The perimeter of the rectangle is: "+ perimeter);
+        */
+        
+        int numberA = 5;
+        int numberB = 3;
+        String result = (numberA>numberB) ? "Number A is the biggest one" : "Number B is the biggest one";
+        System.out.println("result = " + result);
     }
 }
