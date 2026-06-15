8b5b964dc00ed1bc23217e392358576332deb837
Santi Corales <santicorales44@gmail.com>
2026-06-15 17:02:07 -0300

    Agrego lección 3

--- JavaScript/Leccion2/2-ConcatenacionDeCadenas.js
+++ JavaScript/Leccion2/2-ConcatenacionDeCadenas.js
@@ -13,3 +13,21 @@ console.log(together);
 
 name += surname; // Third concatenation using the simplified operator.
 console.log (name);
+
+// Today var is not used anymore, we use let and const.
+let name2 = "Santiago";
+console.log(name2);
+
+const surname2 = "Corales";
+// surname2 = "Corales"; a const cannot be reassigned or modified.
+console.log(surname2);
+
+let x, y; // You can declare multiple variables in a single line.
+x = 17, y = 21; // You can assign values to multiple variables in a single line.
+let z = x + y; // We assign the value of the operation.
+console.log(z);
+let _1num = 31;
+let breaking = "Break"; // Don't use reserved words as variable names.
+
+console.log(_1num);
+console.log(breaking);
---
+++ JavaScript/Leccion3/3-OperadoresJavaScript.js
@@ -0,0 +1,17 @@
+// Exercise to find even numbers
+let number = 10;
+if (number % 2 === 0){
+    console.log("The number is even")
+}
+else{
+    console.log("The number is odd")
+}
+
+// Exercise of legal age
+let age = 18;
+if (age >= 18){
+    console.log("You are an adult")
+}
+else{
+    console.log("You are a minor")
+}
